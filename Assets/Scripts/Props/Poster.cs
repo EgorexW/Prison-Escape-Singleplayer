b875e86e0797d92ea2798c5effa1d962e1b0dd8b
EgorexW <olko.wiktor.10@gmail.com>
2025-09-19 20:31:46 +0200

    Posters into materials

---
+++ Assets/Scripts/Props/Poster.cs
@@ -0,0 +1,21 @@
+using System;
+using Nrjwolf.Tools.AttachAttributes;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Poster : MonoBehaviour
+{
+    [BoxGroup("References")] [Required] [SerializeField] Renderer rend;
+
+    [SerializeField] float destroyChance = 0.5f;
+    [SerializeField] Material[] materials;
+
+    void Awake()
+    {
+        if (Random.value < destroyChance){
+            Destroy(gameObject);
+        }
+        rend.material = materials[UnityEngine.Random.Range(0, materials.Length)];
+    }
+}
