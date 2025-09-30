ee308d1963963af865fcf7c3e2c452c8612e5b4d
EgorexW <olko.wiktor.10@gmail.com>
2025-09-30 09:45:07 +0200

    Builds

---
+++ Assets/Scripts/UI/Settings.cs
@@ -0,0 +1,43 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Settings : MonoBehaviour
+    {
+        [BoxGroup("References")][Required][SerializeField] GameObject settingsMenu;
+        
+        void Awake()
+        {
+            var inputActions = GetComponent<PlayerInput>().actions.FindActionMap("Player");
+            inputActions.FindAction("Settings").performed += ToggleSettings;
+        }
+
+        void ToggleSettings(InputAction.CallbackContext obj)
+        {
+            if (!obj.performed){
+                return;
+            }
+            if (settingsMenu.activeSelf){
+                Unpause();
+            }
+            else{
+                Pause();
+            }
+        }
+
+        void Unpause()
+        {
+            settingsMenu.SetActive(false);
+            Time.timeScale = 1f;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        void Pause()
+        {
+            settingsMenu.SetActive(true);
+            Time.timeScale = 0f;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
