179764754f4a5ec5254c1fe6e94b2214cbf55f8f
EgorexW <olko.wiktor.10@gmail.com>
2025-11-18 16:37:33 +0100

    SpawnTable utils

--- Assets/Scripts/Base/SpawnTable.cs
+++ Assets/Scripts/Base/SpawnTable.cs
@@ -1,12 +1,23 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Sirenix.OdinInspector;
+using Sirenix.Utilities;
+using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 [CreateAssetMenu]
 [InlineEditor]
-public class SpawnTable : ScriptableObject, IGetGameObject
+public class SpawnTable : ScriptableObject
 {
     [SerializeField] List<ObjectWithValue<Object>> gameObjects;
+    
+#if UNITY_EDITOR
+    [ShowInInspector][ReadOnly] List<GameObject> possibleGameObjects;
+    [ShowInInspector][ReadOnly] List<SpawnTable> referencedByTables;
+#endif
 
     public GameObject GetGameObject()
     {
@@ -17,15 +28,42 @@ public class SpawnTable : ScriptableObject, IGetGameObject
         if (rolledObj.Object is GameObject){
             return rolledObj.Object as GameObject;
         }
-        if (rolledObj.Object is IGetGameObject getGameObject){
+        if (rolledObj.Object is SpawnTable getGameObject){
             return getGameObject.GetGameObject();
         }
-        Debug.LogError("Object is not GameObject or IGetGameObject is " + name, this);
+        Debug.LogError("Object is not GameObject or another SpawnTable is " + name, this);
         return null;
     }
-}
-
-public interface IGetGameObject
-{
-    public GameObject GetGameObject();
+    
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        possibleGameObjects.Clear();
+        Queue<ObjectWithValue<Object>> toProcess = new(gameObjects);
+        while (toProcess.Count > 0){
+            var item = toProcess.Dequeue();
+            switch (item.Object){
+                case SpawnTable getGameObject:
+                    if (!getGameObject.referencedByTables.Contains(this)){
+                        getGameObject.referencedByTables.Add(this);
+                    }
+                    foreach (var obj in getGameObject.gameObjects){
+                        toProcess.Enqueue(obj);
+                    }
+                    break;
+                case GameObject gameObj:
+                    if (!possibleGameObjects.Contains(gameObj)){
+                        possibleGameObjects.Add(gameObj);
+                    }
+                    break;
+            }
+        }
+        
+        foreach (var table in referencedByTables.Copy()){
+            if (table.gameObjects.All(x => x.Object != this)){
+                referencedByTables.Remove(table);
+            }
+        }
+    }
+#endif
 }
