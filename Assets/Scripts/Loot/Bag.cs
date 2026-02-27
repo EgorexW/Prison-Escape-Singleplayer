c9b52161d4b623831304766d98790ad7b59f53bb
EgorexW <olko.wiktor.10@gmail.com>
2026-02-27 12:08:05 +0100

    Added weight to items

--- Assets/Scripts/Loot/Bag.cs
+++ Assets/Scripts/Loot/Bag.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class Bag : Equipment
 {
     [SerializeField] int sizeIncrease = 1;
+    [SerializeField] float weightReduction = 0.5f;
 
     protected override void Apply(Player player)
     {
         player.GetInventory().IncreaseSize(sizeIncrease);
+        player.GetInventory().weightReduction += weightReduction;
     }
 }
