9ff7d76febac2bb1ec8b7a72b6a55198a6f24b89
EgorexW <olko.wiktor.10@gmail.com>
2026-08-06 17:02:57 +0200

    Update material and shader properties, refactor Keycard visuals

--- Assets/Scripts/KeycardVisualsNew.cs
+++ Assets/Scripts/KeycardVisualsNew.cs
@@ -1,18 +1,12 @@
 using Sirenix.OdinInspector;
 using UnityEngine;
-using TMPro;
 
 public class KeycardVisualsNew : MonoBehaviour
 {
     [BoxGroup("References")] [Required] public Keycard keycard;
-    
-    [Header("Components")]
-    [SerializeField] private Renderer cardRenderer;
-    [SerializeField] private TMP_Text titleText;
+    [SerializeField] private Renderer stripeRenderer;
 
-    // We cache property IDs for speed
-    private static readonly int StripeColorID = Shader.PropertyToID("_Stripe_Color");
-    // private static readonly int BaseColorID = Shader.PropertyToID("_BaseColor");
+    private static readonly int BaseColorID = Shader.PropertyToID("_BaseColor");
 
     private MaterialPropertyBlock _propBlock;
 
@@ -21,35 +15,15 @@ public class KeycardVisualsNew : MonoBehaviour
         _propBlock = new MaterialPropertyBlock();
     }
 
-    void SetupAutomatic(){
-        SetupCard(keycard.accessLevel.displayName, keycard.accessLevel.color);
-    }
-    
     private void Start()
     {
-        SetupAutomatic();
+        SetStripeColor(keycard.accessLevel.color);
     }
 
-
-    /// <summary>
-    /// Call this when generating a card at runtime!
-    /// </summary>
-    public void SetupCard(string cardName, Color stripeColor)
+    public void SetStripeColor(Color color)
     {
-        // 1. Update text via TextMeshPro 3D
-        if (titleText != null)
-        {
-            titleText.text = cardName;
-        }
-
-        // 2. Fetch current property block from renderer
-        cardRenderer.GetPropertyBlock(_propBlock);
-
-        // 3. Inject custom colors into the block
-        _propBlock.SetColor(StripeColorID, stripeColor);
-        // _propBlock.SetColor(BaseColorID, cardBodyColor);
-
-        // 4. Push the custom properties back to the card (Zero Material Creation!)
-        cardRenderer.SetPropertyBlock(_propBlock);
+        stripeRenderer.GetPropertyBlock(_propBlock);
+        _propBlock.SetColor(BaseColorID, color);
+        stripeRenderer.SetPropertyBlock(_propBlock);
     }
 }
