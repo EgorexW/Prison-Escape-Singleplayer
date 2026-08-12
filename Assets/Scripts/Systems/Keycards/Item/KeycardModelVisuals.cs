35b7530b259c6a7f2884f9136ee3f376eaa5b9b3
EgorexW <olko.wiktor.10@gmail.com>
2026-08-12 18:20:23 +0200

    Enhance KeycardModelVisuals by modifying name handling and improving text and color settings

--- Assets/Scripts/Systems/Keycards/Item/KeycardModelVisuals.cs
+++ Assets/Scripts/Systems/Keycards/Item/KeycardModelVisuals.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -10,6 +11,8 @@ public class KeycardModelVisuals : MonoBehaviour
     [SerializeField] GameObject textParent;
 
     private static readonly int BaseColorID = Shader.PropertyToID("_BaseColor");
+    private static readonly int EmissionColorID = Shader.PropertyToID("_EmissionColor");
+
 
     private MaterialPropertyBlock _propBlock;
 
@@ -22,11 +25,16 @@ public class KeycardModelVisuals : MonoBehaviour
     {
         SetStripeColor(keycard.accessLevel.color);
         SetOneUse(keycard.oneUse, keycard.accessLevel.color);
+        var displayName = ModifyName();
+        SetText(displayName);
+    }
+
+    string ModifyName(){
         var displayName = keycard.accessLevel.displayName;
         if (keycard.oneUse){
             displayName += " Pass";
         }
-        SetText(displayName);
+        return displayName;
     }
 
     void SetOneUse(bool oneUse, Color stripeColor)
@@ -42,10 +50,11 @@ public class KeycardModelVisuals : MonoBehaviour
             Color contrastColor = luminance < 0.5f ? Color.white : Color.black;
 
             // 3. Apply to property block
-            var iconRenderer = oneUseIcon.GetComponent<Renderer>();
-            iconRenderer.GetPropertyBlock(_propBlock);
-            _propBlock.SetColor(BaseColorID, contrastColor);
-            iconRenderer.SetPropertyBlock(_propBlock);
+            foreach (var iconRenderer in oneUseIcon.GetComponentsInChildren<Renderer>()){
+                iconRenderer.GetPropertyBlock(_propBlock);
+                _propBlock.SetColor(BaseColorID, contrastColor);
+                iconRenderer.SetPropertyBlock(_propBlock);
+            }
         } 
         else 
         {
@@ -54,16 +63,26 @@ public class KeycardModelVisuals : MonoBehaviour
     }
 
     void SetText(string displayName){
-        var textMesh = textParent.GetComponentInChildren<TMPro.TextMeshProUGUI>();
-        if (textMesh != null){
+        foreach (var textMesh in textParent.GetComponentsInChildren<TMPro.TextMeshPro>()){
             textMesh.text = displayName;
+            
         }
+        
     }
 
     public void SetStripeColor(Color color)
     {
         stripeRenderer.GetPropertyBlock(_propBlock);
+    
+        // 1. Set the base color
         _propBlock.SetColor(BaseColorID, color);
+    
+        // 2. Calculate -4 EV intensity (color * 2^-4)
+        Color emissionColor = color * Mathf.Pow(2f, -4f);
+    
+        // 3. Set the emission color property
+        _propBlock.SetColor(EmissionColorID, emissionColor);
+    
         stripeRenderer.SetPropertyBlock(_propBlock);
     }
 }
